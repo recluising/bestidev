179444739c21dad96c6db58375c6bbcd5b9d3c34
Fernando Izquierdo-Carrasco <fer.izquierdo@gmail.com>
2011-01-27 23:52:43 +0100

    Added copyright notice

--- legal/lib/legal_hooks.rb
+++ legal/lib/legal_hooks.rb
@@ -1,7 +1,7 @@
 class LegalHooks < Spree::ThemeSupport::HookListener
   #insert after, insert before, replace
   # custom hooks go here
-	replace :footer_left, :text => '<p>trabajo realizado por</p>'
+	replace :footer_left, :text => '<p>&copy; 2011 Bestiaz Retailer S.L.</p>'
 	replace :footer_right, 'shared/legal_footer_right'
 	insert_before :sidebar, 'shared/languages_header'
         replace :admin_product_form_right,'shared/admin_product_form_right_h'
