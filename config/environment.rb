1df2dd1e029990ba51686b809c36ebc3ed02ec1e
Fernando Izquierdo-Carrasco <fer.izquierdo@gmail.com>
2011-01-27 22:57:08 +0100

    make changes for development

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,11 @@
 # Load the rails application
 #ENV["GEM_PATH"]="/home/alejandr/ruby/gems:/usr/local/lib/ruby/gems/1.8:/home/alejandr/ruby/gems/bundler"
 
-ENV["GEM_PATH"]="/home/alejandr/ruby/gems"
-ENV["RAILS_ENV"]="production"
+#ENV["GEM_PATH"]="/home/alejandr/ruby/gems"
+#ENV["RAILS_ENV"]="production"
+ENV["RAILS_ENV"]="development"
 
 require File.expand_path('../application', __FILE__)
-Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+#Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 # Initialize the rails application
 Onlineshop::Application.initialize!
--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -3,11 +3,10 @@ module SpreeSite
     def self.activate
       # Add your custom site logic here
 	Spree::Config.set(:stylesheets => 'screen,besti,homepager')
-        Spree::Config.set(:allow_ssl_in_production => false) 
+        #Spree::Config.set(:allow_ssl_in_production => false) 
 	Spree::Config.set(:default_locale => 'es')
 	Spree::Config.set(:default_country_id => Country.iso_name_like("spain").first.id)
 	Spree::Config.set(:logo => "/images/logo.png")
-
     end
     config.to_prepare &method(:activate).to_proc
   end
