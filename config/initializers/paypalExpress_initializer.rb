05170ca728f295b5129e5d2d8788bc8cb9b47861
Fernando Izquierdo-Carrasco <fer.izquierdo@gmail.com>
2011-03-11 23:26:16 +0100

    forgot this stuff from the paypal express commit

---
+++ config/initializers/paypalExpress_initializer.rb
@@ -0,0 +1,6 @@
+ActiveMerchant::Billing::PaypalExpressGateway.default_currency = 'EUR' # whichever currency code you want 
+#BillingIntegration::PaypalExpress.class_eval do
+# def paypal_site_opts
+#   { :currency => 'EUR' } # whichever currency code you want
+# end
+#end
