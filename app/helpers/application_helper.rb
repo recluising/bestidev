a9f35a3532abab644d19a9419b4cec818bfecc5e
Fernando Izquierdo-Carrasco <fer.izquierdo@gmail.com>
2011-05-13 00:06:45 +0200

    added helper to find a nav. taxon, view it as a pic in the homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def taxon(name)
+    # Shop by Productos, carefull if we change Taxonomy!
+    #Taxonomy.find_by_name("Productos").root.children.find_by_name(name)
+    # Shop by first option, less likely to chanche:
+    Taxonomy.all.first.root.children.find_by_name(name)
+  end
 end
